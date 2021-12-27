8ac8b01ccfe5bdd7c59346c9e5f8f1ee8fffcb2b
ClementRoger <clementrger@gmail.com>
2021-12-27 22:35:40 +0100

    median_list fait. Tous les tests passés

--- funcs.py
+++ funcs.py
@@ -19,3 +19,16 @@ def mean_list(tab):
 	somme = sum(tab)
 	taille = len(tab)
 	return somme / taille
+
+def median_list(tab):
+
+	tab = sorted(tab)
+	taille = len(tab)
+	center_left_index = int( ( (taille - 1) / 2 ) )
+	center_right_index = int( ( (taille + 1) / 2 ) )
+
+	if taille < 1:
+		return None
+	elif taille % 2 == 0:
+		return ( tab[ center_left_index ] + tab[ center_right_index ] ) / 2.0 #tableau de taille paire on renvoie la moyenne des deux éléments centraux
+	return tab[ center_left_index ]	
