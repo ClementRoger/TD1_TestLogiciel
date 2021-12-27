6dfd1b3fe59235b3605770eb9c2b991be86b2fe9
ClementRoger <clementrger@gmail.com>
2021-12-27 22:16:30 +0100

    Initial commit

---
+++ funcs.py
@@ -0,0 +1,8 @@
+import math
+
+def max_int(a,b):
+
+	if a < b :
+		return b
+	else :
+		return a
---
+++ tests_funcs.py
@@ -0,0 +1,14 @@
+import funcs
+import unittest
+import math
+
+class TestFuncs(unittest.TestCase):
+
+	def test_max_int(self):
+		self.assertEqual( funcs.max_int(0 , 2) , 2 )
+		self.assertEqual( funcs.max_int(-1 , -5) , -1 )
+		self.assertEqual( funcs.max_int(-1 , 2) , 2 )
+		self.assertEqual( funcs.max_int(0 , 0) , 0 )
+
+if __name__ == '__main__':
+	unittest.main()
