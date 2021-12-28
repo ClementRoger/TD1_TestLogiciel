a1980255bc1dbab6416d116e6abb3da1287e9be0
ClementRoger <clementrger@gmail.com>
2021-12-28 23:53:30 +0100

    Tests check_password faits

--- tests_funcs_sql.py
+++ tests_funcs_sql.py
@@ -20,7 +20,13 @@ class TestFuncs(unittest.TestCase):
 		self.assertEqual( cursor.execute("""SELECT * FROM users WHERE sprivatekey="fghij" """).fetchone() , ("Bobby" , "motdepasse" , "abcde" , "fghij" , "klmno" , "pqrst") )
 		self.assertEqual( cursor.execute("""SELECT * FROM users WHERE epublickey="klmno" """).fetchone() , ("Bobby" , "motdepasse" , "abcde" , "fghij" , "klmno" , "pqrst") )
 		self.assertEqual( cursor.execute("""SELECT * FROM users WHERE eprivatekey="pqrst" """).fetchone() , ("Bobby" , "motdepasse" , "abcde" , "fghij" , "klmno" , "pqrst") )
-		self.assertEqual( cursor.execute("""SELECT * FROM users WHERE username="Bob" """).fetchone() , ("Bob" , "motdepasseresistant" , "key1" , "key2" , "key3" , "key4" ) )		
+		self.assertEqual( cursor.execute("""SELECT * FROM users WHERE username="Bob" """).fetchone() , ("Bob" , "motdepasseresistant" , "key1" , "key2" , "key3" , "key4" ) )
+
+	def test_check_password(self):
+		self.assertEqual( funcs_sql.check_password( cursor , "Bobby" , "motdepasse" ) , True )
+		self.assertEqual( funcs_sql.check_password( cursor , "Bobby" , "azertyuiop" ) , False )
+		self.assertEqual( funcs_sql.check_password( cursor , "Bob" , "motdepasseresistant" ) , True )
+		self.assertEqual( funcs_sql.check_password( cursor , "Bob" , "motdepassepasresistant" ) , False )		
 
 if __name__ == '__main__':
 	unittest.main()
