41a1a0611acb5879a6801b29cddadc365f9a8d36
ClementRoger <clementrger@gmail.com>
2021-12-28 23:29:41 +0100

    Fonction add_user faite. Tous les tests passés

--- funcs_sql.py
+++ funcs_sql.py
@@ -1,3 +1,6 @@
 import math
-import sql3
+import sqlite3
 
+def add_user( cursor , username , password , spublickey , sprivatekey , epublickey , eprivatekey ):
+
+	cursor.execute( "INSERT INTO users(username , password , spublickey , sprivatekey , epublickey , eprivatekey) VALUES(?,?,?,?,?,?)""" , (username , password , spublickey , sprivatekey , epublickey , eprivatekey) )
