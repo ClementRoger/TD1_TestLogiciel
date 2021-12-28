71ea8bb5b081846ad727c1722ff1986fea3bc24e
ClementRoger <clementrger@gmail.com>
2021-12-28 23:54:08 +0100

    Fonction check_password faite. Tous les tests passés

--- funcs_sql.py
+++ funcs_sql.py
@@ -4,3 +4,7 @@ import sqlite3
 def add_user( cursor , username , password , spublickey , sprivatekey , epublickey , eprivatekey ):
 
 	cursor.execute( "INSERT INTO users(username , password , spublickey , sprivatekey , epublickey , eprivatekey) VALUES(?,?,?,?,?,?)""" , (username , password , spublickey , sprivatekey , epublickey , eprivatekey) )
+
+def check_password( cursor , username , password ):
+
+	return ( cursor.execute( """SELECT password FROM users WHERE username=?""" , (username,) ).fetchone() == (password,) )
